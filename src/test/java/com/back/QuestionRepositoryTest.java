9ebbbb8c5597013bc452da24519dffcb3a9a6de1
hansol0118 <wall197944@naver.com>
2026-02-27 12:34:03 +0900

    11,12-add-customJpaMethod

--- src/test/java/com/back/QuestionRepositoryTest.java
+++ src/test/java/com/back/QuestionRepositoryTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -15,34 +17,57 @@ public class QuestionRepositoryTest {
     private QuestionRepository questionRepository;
     @Autowired
     private AnswerRepository answerRepository;
+//
+//    @Test
+//    void t1(){
+//        Question q1 = new Question();
+//        q1.setSubject("Sbb가 무엇");
+//        q1.setContent("무엇이낙");
+//        questionRepository.save(q1);
+//
+//        Answer a1 = new Answer();
+//        a1.setContent("게시판입니다");
+//        a1.setQuestion(q1);
+//        answerRepository.save(a1);
+//
+//        Answer a2 = new Answer();
+//        a2.setContent("나도 몰라요");
+//        a2.setQuestion(q1);
+//        answerRepository.save(a2);
+//    }
+//    @Test
+//    @Transactional
+//    void t2(){
+//        Question q1 = questionRepository.findById(1).get();
+//        System.out.println(q1.getSubject());
+//        System.out.println(q1.getContent());
+//
+//        List<Answer> answers = q1.getAnswerList();
+//        for(Answer a : answers){
+//            System.out.println(a.getContent());
+//        }
+//}
 
     @Test
-    void t1(){
-        Question q1 = new Question();
-        q1.setSubject("Sbb가 무엇");
-        q1.setContent("무엇이낙");
-        questionRepository.save(q1);
+    void t1() {
+        List<Question> all = this.questionRepository.findAll();
+        assertEquals(2, all.size());
 
-        Answer a1 = new Answer();
-        a1.setContent("게시판입니다");
-        a1.setQuestion(q1);
-        answerRepository.save(a1);
-
-        Answer a2 = new Answer();
-        a2.setContent("나도 몰라요");
-        a2.setQuestion(q1);
-        answerRepository.save(a2);
+        Question q = all.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
     }
+
     @Test
-    @Transactional
     void t2(){
         Question q1 = questionRepository.findById(1).get();
-        System.out.println(q1.getSubject());
-        System.out.println(q1.getContent());
+        assertEquals("sbb가 무엇인가요?",q1.getSubject());
+    }
 
-        List<Answer> answers = q1.getAnswerList();
-        for(Answer a : answers){
-            System.out.println(a.getContent());
-        }
+    @Test
+    void t3(){
+        Question q1 = questionRepository.findBySubject("sbb가 무엇인가요?").get();
+
+        assertThat(q1.getId()).isEqualTo(1);
+        assertThat(q1.getContent()).isEqualTo("sbb에 대해서 알고 싶습니다.");
     }
 }
