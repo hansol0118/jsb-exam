1ea60e8e8189f95515f20e4d1c763066dea73ead
hansol0118 <wall197944@naver.com>
2026-02-27 11:01:16 +0900

    8

--- src/main/java/com/back/JsbExamApplication.java
+++ src/main/java/com/back/JsbExamApplication.java
@@ -8,6 +8,24 @@ public class JsbExamApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JsbExamApplication.class, args);
+
+		Answer a = new Answer();
+
+		//자바 방식
+		Question question = a.getQuestion();
+		question.getContent();
+		a.getQuestion().getContent();
+
+		//특정 질문에 달린 모든 답글들
+		//DB 방식
+		//Question q1 = questionRepository.findById(1);
+		//answerRepository.findByQuestionId(q1.getId()) // 특정 질문에 달린 답변들
+
+		// 자바 방식
+		//Question q1 = questionRepository.findById(1);
+		//q1.getAnswerList();
 	}
+
+
 
 }
--- src/main/java/com/back/Question.java
+++ src/main/java/com/back/Question.java
@@ -4,8 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
 @Entity
+@Getter
+
 public class Question {
 
     @Id
